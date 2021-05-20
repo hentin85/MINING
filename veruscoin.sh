1844567a898834db098757ad9c84ac3e8b20c47f
hendri gumolili <71199325+hentin85@users.noreply.github.com>
2021-05-20 15:19:50 +0800

    Update veruscoin.sh

--- veruscoin.sh
+++ veruscoin.sh
@@ -1,10 +1,7 @@
-#!/bin/sh
+#veruscoin
 sudo apt update
 sudo apt install screen -y
 wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
 tar xf hellminer_cpu_linux.tar.gz
 ./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u TGcoZbmjeHReEPXuMeokoFBEwh5Syep4Qx.HENTIN -p x --cpu 3
-while [ 1 ]; do
-sleep 3
-done
-sleep 999
+
